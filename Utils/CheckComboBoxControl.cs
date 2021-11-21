3fa31a90142cdfc6ce485746ff0855797ee5c0f5
Charles Donohue <charles.donohue@gameloft.com>
2021-11-21 16:56:34 -0500

    Filters drop downs improvements

--- Utils/CheckComboBoxControl.cs
+++ Utils/CheckComboBoxControl.cs
@@ -8,30 +8,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 namespace VisualReplayDebugger
 {
     public class CheckComboBoxControl : ComboBox
     {
-        public IEnumerable<string> SelectedItems => this.Items.Cast<CheckBox>().Where(x=>x.IsChecked.Value).Select(x=>x.Content as string);
-        public IEnumerable<string> UnselectedItems => this.Items.Cast<CheckBox>().Where(x=>!x.IsChecked.Value).Select(x=>x.Content as string);
+        public IEnumerable<string> SelectedItems => CheckBoxItems.Where(x=> x.IsChecked.Value).Select(x=>x.Content as string);
+        public IEnumerable<string> UnselectedItems => CheckBoxItems.Where(x=> !x.IsChecked.Value).Select(x=>x.Content as string);
 
         public event Action Changed;
+
+        private IEnumerable<CheckBox> CheckBoxItems => this.Items.Cast<object>().Skip(1).Cast<CheckBox>();
+
+        private bool DoDropdownKeepOpenHack => true;
 
         public CheckComboBoxControl(string label) 
         {
             this.Width = 160;
-            this.StaysOpenOnEdit = true; // Does not work
+            this.StaysOpenOnEdit = true; // Does not work, so as a workaround I'm forcing IsDropDownOpen on clicks
             this.IsEditable = true;
             this.Text = label;
             this.IsReadOnly = true;
-
+            
             this.DropDownClosed += (o,e) => { this.Text = label; }; // Forces label
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (DoDropdownKeepOpenHack)
+            {
+                // Disable popup animation
+                var popup = (Popup)Template.FindName("PART_Popup", this);
+                popup.PopupAnimation = PopupAnimation.None;
+            }
         }
 
         public void SetItems(IEnumerable<string> items, bool selected)
         {
             ClearItems();
+
+            CheckBox all = new() { Content = "All" };
+            all.Click += All_Click;
+            this.Items.Add(all);
+
             foreach (string item in items)
             {
                 CheckBox cbi = new() { Content = item, IsChecked = selected };
@@ -42,16 +64,35 @@ namespace VisualReplayDebugger
 
         public void ClearItems()
         {
-            foreach (var cbi in this.Items.Cast<CheckBox>())
+            if (this.Items.Count > 0)
+            {
+                var all = this.Items[0] as CheckBox;
+                all.Click -= All_Click;
+            }
+
+            foreach (var cbi in CheckBoxItems)
             {
                 cbi.Click -= Cbi_Click;
             }
             this.Items.Clear();
         }
 
+        private void All_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            bool allChecked = (sender as CheckBox).IsChecked.Value;
+
+            foreach ( var cbi in CheckBoxItems)
+            {
+                cbi.IsChecked = allChecked;
+            }
+            Changed?.Invoke();
+            if (DoDropdownKeepOpenHack) IsDropDownOpen = true; // Hack to keep the dropdown open
+        }
+
         private void Cbi_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             Changed?.Invoke();
+            if (DoDropdownKeepOpenHack) IsDropDownOpen = true; // Hack to keep the dropdown open
         }
     }
 }
