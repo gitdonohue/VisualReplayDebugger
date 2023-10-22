f5b8f34cd44e8a868a778aa06cb738d38231e067
Charles Donohue <cdonohue@oc.netease.com>
2023-10-22 12:55:07 -0400

    bigger icons

--- ReplayControls/IconProvider.cs
+++ ReplayControls/IconProvider.cs
@@ -4,13 +4,11 @@
 using System.Windows.Controls;
 using System.Windows.Media;
 using FontAwesome.Sharp;
-//using FontAwesomeIcon = FontAwesome.Sharp.IconChar;
 
 namespace VisualReplayDebugger;
 
 public static class IconProvider
 {
-    public static Image GetIcon(FontAwesome.Sharp.IconChar icon, int width = 14, int height = 14) => new Image() { Source = icon.ToImageSource(Brushes.Black, width) };
-    //public static Image GetIcon(FontAwesomeIcon icon, int width = 14, int height = 14) => new ImageAwesome { Icon = icon, Width = width, Height = height };
+    public static Image GetIcon(FontAwesome.Sharp.IconChar icon, int width = 24, int height = 24) => new() { Source = icon.ToImageSource(Brushes.Black, width) };
 
 }
